72783e1c498352395d2d898e15b4ebb6c46eaab1
CorintoZ <david.moreno92@gmail.com>
2023-01-15 13:56:44 +0100

    Add methods to fetch show data and similar shows

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -4,3 +4,13 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const fetchTrendingUrl = `${BASE_URL}/trending/all/week?api_key=${API_KEY}`;
 
 export const fetchTrending = () => fetch(fetchTrendingUrl).then((res) => res.json());
+
+export const fetchSimilarShows = (showId: string, media_type: string) =>
+  fetch(`${BASE_URL}/${media_type}/${showId}/similar?api_key=${API_KEY}`).then((res) =>
+    res.json(),
+  );
+
+export const fetchShow = (showId: string, mediaType: string) =>
+  fetch(`${BASE_URL}/${mediaType}/${showId}?api_key=${API_KEY}`).then((res) =>
+    res.json(),
+  );
