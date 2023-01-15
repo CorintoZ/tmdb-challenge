d75079e401853775fdcf20674c747a5d10633b4b
CorintoZ <david.moreno92@gmail.com>
2023-01-15 14:02:05 +0100

    Add show context

---
+++ src/contexts/show.context.tsx
@@ -0,0 +1,25 @@
+import { createContext, FC, useState } from 'react';
+
+import { ShowI } from '../models/Show';
+
+export const ShowContext = createContext<ShowContextType | null>(null);
+
+export const ShowProvider: FC = ({ children }) => {
+  const [shows, setShows] = useState<ShowI[]>([]);
+  const getShow = async (showId: number) => {
+    const show = shows.find((show) => show.id === showId);
+    if (!show) return;
+    return show;
+  };
+  return (
+    <ShowContext.Provider value={{ shows, setShows, getShow }}>
+      {children}
+    </ShowContext.Provider>
+  );
+};
+
+export type ShowContextType = {
+  shows: ShowI[];
+  setShows: (shows: ShowI[]) => void;
+  getShow: (showId: number) => Promise<Partial<ShowI> | undefined>;
+};
